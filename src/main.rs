c7ae997d842c8217e036dde18082c074dd950747
RoJaebl <qgs0924@gmail.com>
2024-01-29 17:46:44 +0900

    :hammer: Mandelbro sample code

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,103 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use image::png::PNGEncoder;
+use image::ColorType;
+use num::Complex;
+use std::env;
+use std::{fs::File, str::FromStr};
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let server = HttpServer::new(|| App::new().service(get_index).service(post_gcd));
+fn main() {
+    let args: Vec<String> = env::args().collect();
 
-    println!("Serving on http: //localhost:3000...");
-    server.bind("127.0.0.1:3000")?.run().await
-}
-
-#[get("/")]
-async fn get_index() -> impl Responder {
-    HttpResponse::Ok().content_type("text/html").body(
-        r#"
-        <title>GCD Calculator</title>
-        <form action="/gcd" method="post">
-        <input type="text" name ="n" />
-        <input type="text" name ="m" />
-        <button type="submit">Compute GCD</button>
-        </form>
-    "#,
-    )
-}
-
-#[derive(Deserialize)]
-struct GcdParameters {
-    n: u64,
-    m: u64,
-}
-
-#[post("/gcd")]
-async fn post_gcd(form: web::Form<GcdParameters>) -> impl Responder {
-    if form.n == 0 || form.m == 0 {
-        return HttpResponse::BadRequest()
-            .content_type("text/html")
-            .body("Computing the GCD with zero is boring.");
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
+        std::process::exit(1);
     }
-    let response = format!(
-        "The greatest common divisor of the numbers {} and {} \n is <b>{} </b>\n",
-        form.n,
-        form.m,
-        gcd(form.n, form.m)
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+}
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
+    }
+}
+
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
     );
 
-    HttpResponse::Ok().content_type("text/html").body(response)
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+    }
 }
 
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
         }
-        m = m % n;
     }
-    n
+}
+
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+
+    Ok(())
 }
